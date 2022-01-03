364c38f975f85984645d897310ec3e91df5d8df3
mprotic123 <milan.protic@ts9maj.edu.rs>
2022-01-03 15:22:59 +0100

    Implemented remove function and data save on page reload

--- main.js
+++ main.js
@@ -1,77 +1,53 @@
-let booksList = [];
-let bookInfo = { title: '', author: ''};
-
-
 const bookTitle = document.querySelector('#title');
 const authorName = document.querySelector('#author');
 const addBtn = document.querySelector('#add_btn');
+const books = document.querySelector('.books');
 
+const savedData = localStorage.getItem('savedInput');
 
-// Store data in array
+let booksList = [];
 
-function storeData () {
-
-    const newBook = Object.create(bookInfo);
-    newBook.title = bookTitle.value;
-    newBook.author = authorName.value;
-
-    
-    booksList.push(newBook);
-    
-    
+if(savedData && savedData !== null) {
+    booksList = JSON.parse(savedData);
 }
 
-// Create A table for books (row: title  author) 
+const displayData = () => {
+    books.innerHTML = '';
+    booksList.forEach((value, index) => {
+        books.innerHTML += `
+        <div class="books">
+        <ul>
+            <li class="title">${value.name}</li>
+            <li class="author">${value.author}</li>
+        </ul>
+         <button id="remove" onclick="removeBook(${index});">remove</button>
+         <hr>
+       </div>`;
+    });
+};
 
-const booksCollection = document.createElement("table");
-document.body.appendChild(booksCollection);
+displayData();
 
-const booksCollectionRow = document.createElement("tr");
-const booksTitles = document.createElement("th");
-const booksAuthors = document.createElement("th");
-booksTitles.innerText = 'Title';
-booksAuthors.innerText = 'Author';
+const saveData = () => localStorage.setItem('savedInput', JSON.stringify(booksList));
 
-    
-booksCollection.appendChild(booksCollectionRow);
-booksCollectionRow.appendChild(booksTitles);
-booksCollectionRow.appendChild(booksAuthors);
+const removeBook = (index) => {
+    if(index !== null && index !== undefined) {
+        booksList.splice(index, 1);
+        saveData();
+        displayData();
+    }
+};
 
+removeBook();
 
-//  Display Data
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newData = {
+        name: bookTitle.value,
+        author: authorName.value,
+    };
 
-const form = document.querySelector('#add_books');
-
-form.addEventListener('submit', (e) => {
-
-    e.preventDefault()
-
-    // Add Book Info 
-
-    const addedBook = document.createElement("tr");
-    const addtitle = document.createElement("td");
-    const addauthor = document.createElement("td");
-
-    addtitle.innerText = bookTitle.value;
-    addauthor.innerText = authorName.value;
-
-    booksCollection.appendChild(addedBook);
-    addedBook.appendChild(addtitle);
-    addedBook.appendChild(addauthor);
-
-}
-
-);
-
-// Store data in locale storage
-
-
-form.addEventListener('change', () => {
-
-    const newBook = Object.create(bookInfo);
-    newBook.title = bookTitle.value;
-    newBook.author = authorName.value;
-
-
-    localStorage.setItem('book', JSON.stringify(newBook));
+    booksList.push(newData);
+    saveData();
+    displayData();
 });
